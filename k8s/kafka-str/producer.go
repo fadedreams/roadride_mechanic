808965e970e1298ed9480a5c0cdad080b95494d7
fadedreams <fadedreams@gmail.com>
2025-09-13 13:55:36 -0400

    bs

--- k8s/kafka-str/producer.go
+++ k8s/kafka-str/producer.go
@@ -15,7 +15,8 @@ import (
 
 func main() {
 	// Configuration for Kafka
-	bootstrapServers := "my-cluster-kafka-bootstrap:9092"
+	// bootstrapServers := "my-cluster-kafka-bootstrap:9092"
+	bootstrapServers := "localhost:9092"
 	topic := "my-topic"
 	consumerGroup := "my-consumer-group"
 
